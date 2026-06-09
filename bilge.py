eee6a2bc94b2f277db535f6dbf0d8bd516cbd9eb
kyzenia <kyzenia@pm.me>
2026-06-09 14:56:52 +0300

    first flight

---
+++ bilge.py
@@ -0,0 +1,72 @@
+def readText():
+    try:
+        dottxt = open("text.txt", "r")
+        text = dottxt.read()
+        dottxt.close()
+        return text
+    except:
+        print("Oops! >> bilge.py >> readText() >> except:")
+
+def mainMenu():
+    sharp, blank = "# "*44, ""
+    title = "Welcome To Bilge v0.1"
+    subtitle = "Please select among available options below (1-3):"
+    slc1 = "                 1) Encrypt Text"
+    slc2 = "                 2) Decrypt Text"
+    exit = "                 3) Exit"
+    print(f"\n{sharp}\n"f"#{blank:^85}#\n"f"#{title:^85}#\n"f"#{blank:^85}#\n"f"#{subtitle:^85}#\n"f"#{blank:^85}#\n"f"#{slc1:<85}#\n"
+          f"#{slc2:<85}#\n"f"#{exit:<85}#\n"f"#{blank:^85}#\n"f"{sharp}\n")
+
+def mainMenuSelection():
+    loop, loop1, loop2, loop3 = 1, 1, 1, 1
+    while loop == 1:
+        try:
+            while loop1 == 1:
+                userChoice = int(input("Your selection: "))
+                if userChoice < 1 or userChoice > 3:
+                    print("\nPlease enter a valid selection!\n")
+                    loop1 = 1
+                else:
+                    loop1 = 0
+        except:
+            print("\nPlease enter a valid selection!\n")
+            loop = 1
+        else:
+            loop = 0
+    if userChoice == 2:
+        while loop2 == 1:
+            try:        
+                uKey = int(input("Enter your key: "))
+            except:
+                print("\nPlease enter a valid key")
+                loop2 = 1
+            else:
+                uKey = str(uKey)
+                userKey = [num for num in uKey]
+                loop2 = 0
+        import decrypt
+        decrypt.main(readText(), userKey)
+    elif userChoice == 1:
+        while loop3 == 1:
+            try:        
+                uKey = int(input("Enter your key: "))
+            except:
+                print("\nPlease enter a valid key")
+                loop3 = 1
+            else:
+                uKey = str(uKey)
+                userKey = [num for num in uKey]
+                loop3 = 0
+        import encrypt
+        encrypt.main(readText(), userKey)
+    else:
+        import credits
+        credits.main()
+        input("Have a great day!\nPlease press enter to exit the program...")
+
+def main():
+    mainMenu()
+    mainMenuSelection()
+
+if __name__ == '__main__':
+    main()
---
+++ credits.py
@@ -0,0 +1,25 @@
+def main():
+    sharp, blank = "# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #", ""
+    bir = "                #                "
+    iki = "              #                  "
+    uc = "            #                    "
+    dort = "          #                      "
+    bes = "#       #               #        "
+    alti = "#     #               # #        "
+    yedi = "#   #               #   #   #    "
+    sekiz = "# #               #     # #      "
+    dokuz = "#       #       #       #       #"
+    on = "# #       #           # #     #  "
+    onbir = "#   #       #       #   #   #    "
+    oniki = "#     #       #   #     # #      "
+    onuc = "#       #       #       #        "
+    ondort = "          #     #                "
+    onbes = "            #   #                "
+    onalti = "              # #                "
+    onyedi = "                #                "
+    print(f"\n{sharp}\n"f"#{blank:^85}#\n"f"#{bir:^85}#\n"f"#{iki:^85}#\n"f"#{uc:^85}#\n"f"#{dort:^85}#\n"f"#{bes:^85}#\n"f"#{alti:^85}#\n"f"#{yedi:^85}#\n" \
+          f"#{sekiz:^85}#\n"f"#{dokuz:^85}#\n"f"#{on:^85}#\n"f"#{onbir:^85}#\n"f"#{oniki:^85}#\n"f"#{onuc:^85}#\n"f"#{ondort:^85}#\n"f"#{onbes:^85}#\n" \
+          f"#{onalti:^85}#\n"f"#{onyedi:^85}#\n"f"#{blank:^85}#\n"f"{sharp}\n")
+
+if __name__ == '__main__':
+    main()
